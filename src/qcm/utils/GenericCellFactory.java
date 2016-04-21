3ffe3aafab519fc7e02846caf1ec78287534af9b
apolline.mallet <apolline.mallet@sts-sio-caen.info>
2016-04-21 22:15:04 +0200

    Gestion des domaines

--- src/qcm/utils/GenericCellFactory.java
+++ src/qcm/utils/GenericCellFactory.java
@@ -1,20 +1,31 @@
 package qcm.utils;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 
 public class GenericCellFactory<T>
 		implements Callback<ListView<T>, ListCell<T>> {
 
+	@FXML
+	public Image back = new Image("/views/img/back.png");
+	public ImageView img = new ImageView(back);
+	
 	@Override
 	public ListCell<T> call(ListView<T> param) {
 		return new ListCell<T>() {
 			@Override
 			protected void updateItem(T o, boolean empty) {
 				super.updateItem(o, empty);
-				if (o != null)
+				if (o != null) {
 					setText(o.toString());
+					setGraphic(img);
+				}
 			}
 		};
 	}
